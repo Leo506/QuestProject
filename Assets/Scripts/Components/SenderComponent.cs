50ef934b1b78c4abe034e12a866c59aa5b02227b
Artem Levin <leoartemlev@yandex.ru>
2022-06-13 20:57:32 +0500

    Исправил баг, из-за которого нельзя было начать повторно диалог с отправителем

--- Assets/Scripts/Components/SenderComponent.cs
+++ Assets/Scripts/Components/SenderComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,20 @@ namespace Components
 {
     public class SenderComponent : MonoBehaviour, IUsable
     {
+        private void OnEnable()
+        {
+            DialogSystem.DialogText.DialogEndEvent += OnDialogEnd;
+        }
+
+        private void OnDialogEnd(bool action)
+        {
+            if (action)
+                Destroy(this);
+        }
+
         public void Use()
         {
             DialogSystem.DialogText.Instance.StartDialog(QuestSystem.QuestManager.currentQuestID);
-            Destroy(this);
         }
     }
 }
