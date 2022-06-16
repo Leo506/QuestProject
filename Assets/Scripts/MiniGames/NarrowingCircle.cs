e231b4c41ef3feb41db364ba156e2cea982e5ffd
Artem Levin <leoartemlev@yandex.ru>
2022-06-16 23:18:22 +0500

    Добавил сузающийся круг

---
+++ Assets/Scripts/MiniGames/NarrowingCircle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MiniGames
+{
+    public class NarrowingCircle : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartCoroutine(Narrowing());
+        }
+
+        
+        IEnumerator Narrowing()
+        {
+            int countOfIter = 500;
+            float desc = (transform.localScale.x - 1) / countOfIter;
+            for (int i = 0; i < countOfIter; i++)
+            {
+                var size = transform.localScale;
+                size.x -= desc;
+                size.y -= desc;
+
+                transform.localScale = size;
+
+                yield return new WaitForSeconds(0.001f);
+            }
+        }
+    }
+}
