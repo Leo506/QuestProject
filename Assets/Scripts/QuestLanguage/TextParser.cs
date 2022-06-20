e88d72768294fe5ab6b150970a6a937a933df873
Artem Levin <leoartemlev@yandex.ru>
2022-06-20 20:51:55 +0500

    Переписал парсер ЯОК так, чтобы он не зависел от порядка инструкций

--- Assets/Scripts/QuestLanguage/TextParser.cs
+++ Assets/Scripts/QuestLanguage/TextParser.cs
@@ -13,51 +13,42 @@ namespace QuestLanguage
         public void Parse(string content)
         {
             lines = content.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
-            foreach (var item in lines)
-            {
-                Debug.Log(item);
-            }
         }
 
-        public string[] GetCommands()
-        {
-            string[] toReturn = new string[lines.Length];
 
-            for (int i = 0; i < lines.Length; i++)
+        public object CreateQuest()
+        {
+            object toReturn = null;
+
+            foreach (var line in lines)
             {
-                toReturn[i] = lines[i].Split(new char[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries)[0];
+                List<string> words = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                string type = words[0];
+
+                TextInfo tmp = new CultureInfo("en-US", false).TextInfo;
+
+                if (IsQuestCommand(words[0]))
+                {
+                    type = "QuestLanguage." + tmp.ToTitleCase(type) + "Quest";
+                    Type t = Type.GetType(type);
+
+                    toReturn = Activator.CreateInstance(t, new object[] { line.Remove(0, words[0].Length) });
+                }
+                else
+                {
+                    type = "QuestLanguage." + tmp.ToTitleCase(type);
+                    Type t = Type.GetType(type);
+                    Activator.CreateInstance(t, new object[] { line.Remove(0, words[0].Length) });
+                }
             }
 
             return toReturn;
         }
 
-        public object CreateQuest()
+        private bool IsQuestCommand(string command)
         {
-            string type = lines[0].Split(new char[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries)[0];
-            lines[0] = lines[0].Remove(0, type.Length + 1);
-            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
-            type = "QuestLanguage." + myTI.ToTitleCase(type) + "Quest";
-            Debug.Log("Type: " + type);
-            System.Type t = System.Type.GetType(type);
-
-            ExecuteAdditional();
-
-            return Activator.CreateInstance(t, new object[] { lines[0] });
-        }
-
-        private void ExecuteAdditional()
-        {
-            for (int i = 1; i < lines.Length; i++)
-            {
-                string type = lines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[0];
-                var newLine = lines[i].Remove(0, type.Length + 1);
-
-                TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
-                type = "QuestLanguage." + myTI.ToTitleCase(type);
-
-                var t = Type.GetType(type);
-                Activator.CreateInstance(t, new object[] { newLine });
-            }
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            return Type.GetType("QuestLanguage." + textInfo.ToTitleCase(command) + "Quest") != null;
         }
     }
 }
