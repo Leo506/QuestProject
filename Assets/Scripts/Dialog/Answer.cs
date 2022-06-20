ebe459bb1e51f4cc0be7b85521d1a2a8834d860c
Artem Levin <leoartemlev@yandex.ru>
2022-06-20 10:21:59 +0500

    Накидал новую структуру xml-документа; написал новые классы, которые лягут в основу новой системы

---
+++ Assets/Scripts/Dialog/Answer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogSystem
+{
+    public class Answer
+    {
+        public string Text { get; set; }
+
+        public Phrase Next { get; set; }
+
+        public bool Exit = false;
+    }
+}
