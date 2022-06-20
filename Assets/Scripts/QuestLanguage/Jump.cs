f9ddd4ec43fccda4403288e82214b1fac71c2b7c
Artem Levin <leoartemlev@yandex.ru>
2022-06-20 21:56:56 +0500

    Добавил новую инструкцию в ЯОК - переход на опр сцену

---
+++ Assets/Scripts/QuestLanguage/Jump.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace QuestLanguage
+{
+    public class Jump
+    {
+        public Jump(string parametr)
+        {
+            List<string> parList = parametr.Split(new char[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var index = parList.FindIndex(s => s == "to");
+
+            parList[index + 2] = parList[index + 2].Trim();
+            Debug.Log("TestScene length " + "TestScene".Length);
+            Debug.Log("parList[index + 2].Length == \"TestScene\".Length " + (parList[index + 2].Length == "TestScene".Length));
+            SceneManager.LoadScene(parList[index + 2]);
+        }
+    }
+}
