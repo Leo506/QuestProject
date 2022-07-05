1b12b63c736d3ff1110ce248af6bdf44d910e243
Artem <leoartemlev@yandex.ru>
2022-07-05 11:18:41 +0500

    Добавил объект записи, котору можно подобрать

---
+++ Assets/Scripts/Notes/NoteObj.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Notes
+{
+    public class NoteObj : MonoBehaviour, IUsable
+    {
+        public int NoteIndex;
+
+        public void Use()
+        {
+            Destroy(this.gameObject);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag != "Player")
+                return;
+
+            var player = other.gameObject.GetComponent<Player.PlayerLogic>();
+
+            player.SetUsableObj(this);
+        }
+
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag != "Player")
+                return;
+
+
+            var player = other.gameObject.GetComponent<Player.PlayerLogic>();
+
+            player.UnsetUsableObj();
+        }
+    }
+}
